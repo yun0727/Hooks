72ec0a5c58f46f4403ab6e45ea1cb1290b6cd7f3
yun <austru00@gmail.com>
2024-03-06 19:40:02 +0900

    useTitle

--- src/App.js
+++ src/App.js
@@ -2,17 +2,10 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 
-
 function App() {
-  const sayHello=()=>console.log("Hello")
-  const [number, setNumber] = useState(0);
-  const [aNumber, setAnumber] = useState(0);
-  useEffect(sayHello, [])
   return (
     <div className="App">
       <div>Hi</div>
-      <button onClick={()=>setNumber(number+1)} />
-      <button onClick={()=>setAnumber(aNumber+1)} />
     </div>
   );
 }
