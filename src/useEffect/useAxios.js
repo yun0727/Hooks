2b29c9c8801366ff8635fbdb809b3bb40f0cd61a
yun <austru00@gmail.com>
2024-03-08 16:06:13 +0900

    useAxios

---
+++ src/useEffect/useAxios.js
@@ -0,0 +1,35 @@
+import defaultAxios from "axios";
+import { useState, useEffect } from "react";
+
+export const useAxios = (opts, axiosInstance = defaultAxios) => {
+  useEffect(() => {
+    axiosInstance(opts)
+      .then(data => {
+        setState({
+          ...state,
+          loading: false,
+          data
+        });
+      })
+      .catch(error => {
+        setState({ ...state, loading: false, error });
+      });
+  }, [trigger]);
+  const [state, setState] = useState({
+    loading: true,
+    error: null,
+    data: null
+  });
+  const [trigger, setTrigger] = useState(0);
+  if (!opts.url) {
+    return;
+  }
+  const refetch = () => {
+    setState({
+      ...state,
+      loading: true
+    });
+    setTrigger(Date.now());
+  };
+  return { ...state, refetch };
+};
